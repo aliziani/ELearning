94ffb213c685fee1bba387b1b0fad8edb59a37e2
Ali Ziani <Ali.Ziani@nsi-sa.be>
2016-04-06 16:49:04 +0200

    Configure authentification with token (+ cors)

--- TokenAuthentification/Startup.cs
+++ TokenAuthentification/Startup.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
+using System;
 using System.Web.Http;
+using TokenAuthentification.App_Start;
 
 [assembly: OwinStartup(typeof(TokenAuthentification.Startup))]
 namespace TokenAuthentification
@@ -9,9 +12,31 @@ namespace TokenAuthentification
     {
         public void Configuration(IAppBuilder app)
         {
+            // OAuth
+            ConfigureOAuth(app);
+
+            // Web Api
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config);
             app.UseWebApi(config);
+
+            // Activate CORS
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,                
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
         }
 
     }
