9fec2103a305e6a6cdb7d080c7af8a81100472ef
Ali Ziani <Ali.Ziani@nsi-sa.be>
2016-04-06 14:50:23 +0200

    Add default format (json) in web api config

--- TokenAuthentification/App_Start/WebApiConfig.cs
+++ TokenAuthentification/App_Start/WebApiConfig.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json.Serialization;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace TokenAuthentification
@@ -19,6 +19,10 @@ namespace TokenAuthentification
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Default format
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
